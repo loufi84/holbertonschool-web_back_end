69928e34866a959a0e55e23ae6c17de10440c75d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 09:38:32 +0200

    Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a single method that floor a number
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This methos will floor a number
+    """
+    return math.floor(n)
