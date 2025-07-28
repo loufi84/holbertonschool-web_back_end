9895814b377b6d39e2b286a9fdd845d317ed2c4c
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 10:30:54 +0200

    Task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides an async coroutine that wait and return a random delay
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    The said method
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ python_async_function/README.md
@@ -0,0 +1,1 @@
+This repo contains tasks about async functions in Python
