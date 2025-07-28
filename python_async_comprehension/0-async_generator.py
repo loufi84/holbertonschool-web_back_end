868927095619671e0cfc728966ff9f735043acd8
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 13:43:31 +0200

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     The said method
     """
