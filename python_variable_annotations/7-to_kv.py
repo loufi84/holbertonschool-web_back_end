956eec349c7d50b6700839199dfbbb4b1adab6f0
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 10:14:20 +0200

    Task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a function that returns a tuple with a
+string and the square of a number
+"""
+from typing import Union, Tuple
+
+
+def to_kev(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+        Takes a string k and an int or float v, returns a tuple with k and
+        the square of v
+    """
+    return (k, v * v)
