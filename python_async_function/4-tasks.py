322b691062488db6c64b831fca2540134e73faa3
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 11:38:08 +0200

    Task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+A module that contains a method
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    The said method
+    """
+    delays: List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
+
+    return delays
