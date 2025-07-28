9109dd864282f158fce8a7eab7ae373117e2e1a0
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 10:18:19 +0200

    Task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a method in a method to multiply
+"""
+from typing import Callable
+
+
+def make_multipler(multiplier: float) -> Callable[[float], float]:
+    """
+    The method that call the method to multiply
+    """
+    def multiplier_func(x: float) -> float:
+        """
+        The method that multiply
+        """
+        return x * multiplier
+    return multiplier_func
