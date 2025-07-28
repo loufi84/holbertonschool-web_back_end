d85beda8303e1e821608b706dfc521c8d0ae66e7
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 09:42:46 +0200

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a methos that return the string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This method will take a float and return its string representation
+    """
+    return str(n)
