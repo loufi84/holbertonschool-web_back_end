90db91b8675078f61191b8492d728222868f9704
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 13:48:00 +0200

    Task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """
-A module that provides a method
+This module contains a method
+in this module
 """
-import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List(float):
+async def async_comprehension() -> List[float]:
     """
-    The method
+    The said method
+    in question.
     """
     return [i async for i in async_generator()]
