108e1ce0cb89251b73ebd21e58b91ee2deead2d7
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-28 10:18:54 +0200

    Task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@ This module provides a method in a method to multiply
 from typing import Callable
 
 
-def make_multipler(multiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     The method that call the method to multiply
     """
