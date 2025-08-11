23f5529b20d68ebc015b47713a1c86c494fb3b44
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-11 15:47:47 +0200

    Simplified the code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,23 +11,19 @@ def main():
     The method to provide the stats of logs stored in MongoDB
     """
     client = MongoClient('mongodb://localhost:27017')
-    db = client.logs
-    nginx = db.nginx
+    logs = client.logs.nginx
 
-    total_logs = nginx.count_documents({})
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    total_logs = logs.count_documents({})
     print(f"{total_logs} logs")
-    print("Methods:")
 
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
-        count = nginx.count_documents({"method": method})
+        count = logs.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_checks = nginx.count_documents({"method": "GET", "path": "/status"})
+    status_checks = logs.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_checks} status check")
-
-    client.close()
 
 if __name__ == "__main__":
     main()
