a487a913f1e5ff4f77dc09ff3a064175b603f733
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-11 14:57:52 +0200

    Task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module provides a single method that returns the list of schools
+having a specificntopic.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    The method returning the list.
+    Args:
+        mongo_collection: The collection to target.
+        topic: The topic to search
+    """
+    return mongo_collection.find({"topic": topic})
