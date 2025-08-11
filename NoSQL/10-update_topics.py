b97eaf95229cf7fcb0d0e5370618d258ee6eee5a
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-11 13:08:19 +0200

    Task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains a single method that changes all topics of a school
+document, based on the name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    The method that update the topics.
+    Args:
+        mongo_collection: The collection to target
+        name: The name of the school
+        topics: The topics to add
+    """
+    updated = mongo_collection.update_one(name, topics)
+    return updated
