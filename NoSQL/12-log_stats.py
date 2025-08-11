c93700bf5f08c7c7a1da5bd62c1e93296bc89b47
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-11 15:50:47 +0200

    Refactored once again the code to be simpler. Modified name of function. Change localhost to 127.0.0.1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 """
-This module provides stats about Nginx logs in MongoDB
+Module for providing stats about Nginx logs stored in MongoDB
 """
-
-
 from pymongo import MongoClient
 
-def main():
-    """
-    The method to provide the stats of logs stored in MongoDB
-    """
-    client = MongoClient('mongodb://localhost:27017')
-    logs = client.logs.nginx
 
-    total_logs = logs.count_documents({})
+def log_stats():
+    """
+    The method that provides stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    
+    total_logs = logs_collection.count_documents({})
     print(f"{total_logs} logs")
-
+    
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        count = logs.count_documents({"method": method})
+        count = logs_collection.count_documents({"method": method})
         print(f"    method {method}: {count}")
-
-    status_checks = logs.count_documents({"method": "GET", "path": "/status"})
+    
+    status_checks = logs_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
     print(f"{status_checks} status check")
 
+
 if __name__ == "__main__":
-    main()
+    log_stats() 
