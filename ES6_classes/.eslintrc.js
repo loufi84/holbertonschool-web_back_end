7df45ea707596546b6874359e9be704d1c9de131
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-18 15:55:06 +0200

    Test

---
+++ ES6_classes/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'jest/no-large-snapshots': 'off',
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        rules: {
+        "jest/no-large-snapshots": "off",
+        },
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+  };
