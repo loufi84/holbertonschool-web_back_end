3dd84700ee3f42da7dc0e9ac96f9de61b7afcc4d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-25 11:17:07 +0200

    Task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  return studentsList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
