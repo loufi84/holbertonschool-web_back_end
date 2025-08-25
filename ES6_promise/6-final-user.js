12d9d9a69195e54dd113e5f9638ca54698018141
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-25 14:40:50 +0200

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fullfilled' ? result.value : result.reason
+      value: result.status === 'fulfilled' ? result.value : result.reason
     })));
 }
