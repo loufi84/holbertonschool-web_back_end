a08f0a6baa1f2544d8e1d39cf0e1842c4da647a1
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-25 11:44:32 +0200

    Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
   const res = [];
   set.forEach((item) => {
-    if (item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
       res.push(item.slice(startString.length));
     }
   });
