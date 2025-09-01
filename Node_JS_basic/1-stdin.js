055ba6c2092c30d2908577c85c6dc8cc8c873466
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-01 10:17:44 +0200

    Task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     const name = chunk.trim();
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
