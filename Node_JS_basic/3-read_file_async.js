a15398ae77f38a674d5b51730280bbefd6fde4da
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-01 14:43:26 +0200

    Task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((file) => {
+      const lines = file.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
+
+      console.log(`Number of students: ${students.length}`);
+
+      const groups = {};
+
+      for (const line of student) {
+        const parts = line.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
+
+        if (!groups[field]) {
+          groups[field] = [];
+        }
+
+        for (const field in groups) {
+          if (Object.prototype.hasOwnProperty.call(groups, field)) {
+            const list = groups[field].join(', ');
+            console.log(
+              `Number of students in ${field}; ${groups[field.length]}. List: ${list}`
+            );
+          }
+        }
+      }
+    })
+
+    .catch(() => {
+      return Promise.reject(new Error('Cannot load the databse'));
+    })
+}
+
+module.exports = countStudents;
