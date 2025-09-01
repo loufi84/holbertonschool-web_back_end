26f71ae54a74c22d799058b0fabf48e5dfec8199
Quentin <48597772+loufi84@users.noreply.github.com>
2025-09-01 10:23:01 +0200

    Task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
+  if (process.stdin.isTTY) {
+    process.exit(0);
+  }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
